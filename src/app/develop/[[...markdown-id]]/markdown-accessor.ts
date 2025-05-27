dc841fdb18fec5652abaf220b903b9e3e10351b7
Aaron <aarontburnham@hotmail.com>
2025-05-27 02:49:38 -0700

    Update markdown-accessor.ts

--- src/app/develop/[[...markdown-id]]/markdown-accessor.ts
+++ src/app/develop/[[...markdown-id]]/markdown-accessor.ts
@@ -4,7 +4,7 @@ import * as fs from 'fs';
 import * as path from "path";
 
 
-const DOC_PATH: string = process.cwd() + "/public/docs/";
+const DOC_PATH: string = path.join(process.cwd(), "public", "docs");
 
 
 export type FileTree = {
