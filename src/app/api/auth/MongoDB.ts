35ec0624206a2d04deab60a6d87d01ba03592e36
Aaron <aarontburnham@hotmail.com>
2025-05-28 16:34:43 -0700

    Update MongoDB.ts

--- src/app/api/auth/MongoDB.ts
+++ src/app/api/auth/MongoDB.ts
@@ -1,28 +1,16 @@
 import mongoose from "mongoose";
-
-const { MONGODB_URI } = process.env 
-
-if (!MONGODB_URI) {
-    throw new Error("Please define the MONGODB_URI environment variable");
-}
-
-let cached = (global as any).mongoose || { conn: null, promise: null };
-
-export async function connectDB() {
-    if (cached.conn) {
-        return cached.conn;
+const { MONGODB_URI } = process.env;
+export const connectDB = async () => {
+    try {
+        const { connection } = await mongoose.connect(MONGODB_URI as string);
+        if (connection.readyState === 1) {
+            return Promise.resolve(true);
+        }
+    } catch (error) {
+        console.error(error);
+        return Promise.reject(error);
     }
 
-    if (!cached.promise) {
-        cached.promise = mongoose.connect(MONGODB_URI as string, {
-            bufferCommands: false,
-        }).then((mongoose) => {
-            return mongoose;
-        });
-    }
+};
 
-    cached.conn = await cached.promise;
-    (global as any).mongoose = cached;
 
-    return cached.conn;
-}
